b1a41c86bfce5407c9f87609d0d93b33d00ad492
xjasonlyu <xjasonlyu@gmail.com>
2022-05-01 19:13:39 +0800

    Add FC2

---
+++ provider/fc2.go
@@ -0,0 +1,108 @@
+package provider
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"path"
+	"regexp"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+	"github.com/gocolly/colly/v2/extensions"
+	"github.com/javtube/javtube/model"
+	"github.com/javtube/javtube/util"
+)
+
+var _ Provider = (*FC2)(nil)
+
+type FC2 struct {
+	MovieURL  string
+	SampleURL string
+}
+
+func NewFC2() Provider {
+	return &FC2{
+		MovieURL:  "https://adult.contents.fc2.com/article/%s/",
+		SampleURL: "https://adult.contents.fc2.com/api/v2/videos/%s/sample",
+	}
+}
+
+func (fc2 *FC2) GetMovieInfoByID(id string) (info *model.MovieInfo, err error) {
+	return fc2.GetMovieInfoByLink(fmt.Sprintf(fc2.MovieURL, id))
+}
+
+func (fc2 *FC2) GetMovieInfoByLink(link string) (info *model.MovieInfo, err error) {
+	homepage, err := url.Parse(link)
+	if err != nil {
+		return nil, err
+	}
+
+	info = &model.MovieInfo{
+		ID:            path.Base(homepage.Path),
+		Number:        fmt.Sprintf("FC2-%s", path.Base(homepage.Path)),
+		Homepage:      homepage.String(),
+		Actors:        []string{},
+		PreviewImages: []string{},
+		Tags:          []string{},
+	}
+
+	c := colly.NewCollector(extensions.RandomUserAgent)
+
+	// Headers
+	c.OnXML(`//div[@class="items_article_headerInfo"]`, func(e *colly.XMLElement) {
+		info.Title = e.ChildText(`.//h3`)
+		info.Tags = e.ChildTexts(`.//section[@class="items_article_TagArea"]/div/a`)
+		info.Maker = e.ChildText(`.//ul/li[last()]/a`)
+		{ /* score */
+			class := e.ChildAttr(`.//li[@class="items_article_StarA"]/a/p/span`, "class")
+			info.Score = util.ParseScore(regexp.MustCompile(`(\d+)$`).FindString(class))
+		}
+		{ /* release date */
+			ss := strings.Split(e.ChildText(`.//div[@class="items_article_Releasedate"]/p`), ":")
+			info.ReleaseDate = util.ParseDate(ss[len(ss)-1])
+		}
+	})
+
+	// Summary
+	c.OnXML(`//section[@class="items_article_Contents"]/iframe`, func(e *colly.XMLElement) {
+		d := c.Clone()
+		d.OnXML(`//html/body/div`, func(e *colly.XMLElement) {
+			info.Summary = strings.TrimSpace(e.Text)
+		})
+		d.Visit(e.Request.AbsoluteURL(e.Attr("src")))
+	})
+
+	// Thumb+Cover
+	c.OnXML(`//div[@class="items_article_MainitemThumb"]/span/img`, func(e *colly.XMLElement) {
+		info.ThumbURL = e.Request.AbsoluteURL(e.Attr("src"))
+		info.CoverURL = info.ThumbURL
+	})
+
+	// Preview Images
+	c.OnXML(`//section[@class="items_article_SampleImages"]/ul/li`, func(e *colly.XMLElement) {
+		info.PreviewImages = append(info.PreviewImages, e.Request.AbsoluteURL(e.ChildAttr(`.//a`, "href")))
+	})
+
+	// Preview Video
+	//c.OnScraped(func(r *colly.Response) {
+	//	d := c.Clone()
+	//	d.OnResponse(func(r *colly.Response) {
+	//		data := struct {
+	//			Path string `json:"path"`
+	//			Code int    `json:"code"`
+	//		}{}
+	//		if err := json.Unmarshal(r.Body, &data); err == nil && data.Code == http.StatusOK {
+	//			info.PreviewVideoURL = data.Path
+	//		}
+	//	})
+	//	d.Visit(fmt.Sprintf(fc2.SampleURL, info.ID))
+	//})
+
+	err = c.Visit(info.Homepage)
+	return
+}
+
+func (fc2 *FC2) SearchMovie(keyword string) ([]*model.SearchResult, error) {
+	return nil, errors.New("no search support for FC2")
+}
