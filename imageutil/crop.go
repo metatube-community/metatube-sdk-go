ccf23f58c9ad3f52a4ad065fd1edf376c54df634
xjasonlyu <xjasonlyu@gmail.com>
2022-05-08 14:59:16 +0800

    Add image crop

---
+++ imageutil/crop.go
@@ -0,0 +1,53 @@
+package imageutil
+
+import (
+	"image"
+)
+
+type CropableImage interface {
+	image.Image
+	SubImage(image.Rectangle) image.Image
+}
+
+func CropImage(img image.Image, rect image.Rectangle) image.Image {
+	return img.(CropableImage).SubImage(rect)
+}
+
+const (
+	minRatio = 1e-2
+	maxRatio = 1e2
+)
+
+func CropImagePosition(img image.Image, ratio float64, pos float64) image.Image {
+	if ratio < minRatio || ratio > maxRatio {
+		return img // no cropping
+	}
+	width := img.Bounds().Dx()
+	height := img.Bounds().Dy()
+	var (
+		w, h = width, height
+		x, y = 0, 0 // default
+	)
+	if w = int(float64(height) * ratio); w < width {
+		x = max(min(int(float64(width)*pos)-int(float64(w)/2), width-w), 0)
+	} else if h = int(float64(width) / ratio); h < height {
+		y = max(min(int(float64(height)*pos)-int(float64(h)/2), height-h), 0)
+	}
+	return CropImage(img,
+		image.Rect(0, 0, w, h).
+			Add(image.Pt(x, y)).Add(img.Bounds().Min))
+}
+
+func max[T int | float64](a, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min[T int | float64](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
