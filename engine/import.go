b467ff4b1a55cd1866c00a97287b8da57a1ebc21
xjasonlyu <xjasonlyu@gmail.com>
2022-05-10 15:38:43 +0800

    Add engine/import.go

---
+++ engine/import.go
@@ -0,0 +1,19 @@
+package engine
+
+// Register Providers
+import (
+	// Actor
+	_ "github.com/javtube/javtube-sdk-go/provider/gfriends"
+	_ "github.com/javtube/javtube-sdk-go/provider/xslist"
+	// Movie
+	_ "github.com/javtube/javtube-sdk-go/provider/1pondo"
+	_ "github.com/javtube/javtube-sdk-go/provider/airav"
+	_ "github.com/javtube/javtube-sdk-go/provider/caribbeancom"
+	_ "github.com/javtube/javtube-sdk-go/provider/fanza"
+	_ "github.com/javtube/javtube-sdk-go/provider/fc2"
+	_ "github.com/javtube/javtube-sdk-go/provider/heyzo"
+	_ "github.com/javtube/javtube-sdk-go/provider/javbus"
+	_ "github.com/javtube/javtube-sdk-go/provider/mgstage"
+	_ "github.com/javtube/javtube-sdk-go/provider/sod"
+	_ "github.com/javtube/javtube-sdk-go/provider/tokyo-hot"
+)
