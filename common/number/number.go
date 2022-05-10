1abc64f2b9161e76a05f61c74b80ab98f5feadc2
xjasonlyu <xjasonlyu@gmail.com>
2022-05-11 01:36:16 +0800

    add common/number

---
+++ common/number/number.go
@@ -0,0 +1,41 @@
+package number
+
+import (
+	"path"
+	"regexp"
+	"strings"
+	"unicode"
+)
+
+func findFirstNonASCII(s string) int {
+	for i, r := range []rune(s) {
+		if r > unicode.MaxASCII {
+			return i
+		}
+	}
+	return len(s)
+}
+
+func Trim(s string) string {
+	s = s[:len(s)-len(path.Ext(s))] // trim extension
+	if ss := strings.SplitN(s, "@", 2); len(ss) == 2 {
+		s = ss[1] // trim @ char
+	}
+	s = regexp.MustCompile(`(?i)[-_\s](fhd|hd|sd|1080p|720p|4K|x264|x265|uncensored|leak)`).
+		ReplaceAllString(s, "") // trim tags
+	s = regexp.MustCompile(`^(?i)(cari|carib|caribean|1Pondo|heydouga|pacopacomama|muramura|Tokyo.*Hot)[-_\s]`).
+		ReplaceAllString(s, "") // trim prefixes
+	s = s[:findFirstNonASCII(s)] // trim unicode content
+	s = strings.Fields(s)[0]     // trim possible alpha started title
+	for re := regexp.MustCompile(`(?i)([-_](c|cd\d{1,2})|ch)\s*$`); re.MatchString(s); {
+		s = re.ReplaceAllString(s, "") // repeatedly trim suffixes
+	}
+	return strings.TrimSpace(s)
+}
+
+// IsUncensored returns true if the number is belonged to uncensored movie.
+func IsUncensored(s string) bool {
+	return regexp.
+		MustCompile(`^(?i)[\d-]{4,}|\d{6}_\d{2,3}|(cz|gedo|k|n|kb|red-|se)\d{2,4}|heyzo.+|fc2-.+|xxx-av-.+|heydouga-.+$`).
+		MatchString(s)
+}
---
+++ common/number/number_test.go
@@ -0,0 +1,64 @@
+package number
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTrim(t *testing.T) {
+	for _, unit := range []struct {
+		orig string
+		want string
+	}{
+		{"ABP-030", "ABP-030"},
+		{"ABP-030-C", "ABP-030"},
+		{"ABP-030-C.mp4", "ABP-030"},
+		{"ABP-358_C.mkv", "ABP-358"},
+		{"ABP-030-C-c_c-C-Cd1-cd4.mp4", "ABP-030"},
+		{"rctd-460ch.mp4", "rctd-460"},
+		{"rctd-461-C-cD4.mp4", "rctd-461"},
+		{"rctd-461-cd3.mp4", "rctd-461"},
+		{"rctd-461-Cd3-C.mp4", "rctd-461"},
+		{"rctd-461_Cd39-C.mp4", "rctd-461"},
+		{"ssis00123.mp4", "ssis00123"},
+		{"SDDE-625_uncensored_C.mp4", "SDDE-625"},
+		{"SDDE-625_uncensored_leak_C.mp4", "SDDE-625"},
+		{"SDDE-625_uncensored_leak_C_cd1.mp4", "SDDE-625"},
+		{"GIGL-677_4K.mp4", "GIGL-677"},
+		{"093021_539-FHD.mkv", "093021_539"},
+		{"sbw99.cc@iesp-653-4K.mp4", "iesp-653"},
+		{"jav20s8.com@GIGL-677.mp4", "GIGL-677"},
+		{"jav20s8.com@GIGL-677_4K.mp4", "GIGL-677"},
+		{"133ARA-030你好.mp4", "133ARA-030"},
+		{"133ARA-030 你好.mp4", "133ARA-030"},
+		{"133ARA-030 hello there.mp4", "133ARA-030"},
+		{"133ARA-030-C 你好.mp4", "133ARA-030"},
+		{"test.xxx@133ARA-030 你好.mp4", "133ARA-030"},
+		{"Tokyo Hot n9001 FHD.mp4", "n9001"},
+		{"TokyoHot-n1287-HD .mp4", "n1287"},
+		{"caribean-020317_001.mp4", "020317_001"},
+		{"heydouga-4102-023-CD2.iso", "4102-023"},
+	} {
+		assert.Equal(t, unit.want, Trim(unit.orig), unit.orig)
+	}
+}
+
+func TestIsUncensored(t *testing.T) {
+	for _, unit := range []struct {
+		orig string
+		want bool
+	}{
+		{"ABP-030", false},
+		{"ssis00123", false},
+		{"133ARA-030", false},
+		{"FC2-738573", true},
+		{"123456_789", true},
+		{"123456-789", true},
+		{"heyzo-1342", true},
+		{"n1342", true},
+		{"kb1342", true},
+	} {
+		assert.Equal(t, unit.want, IsUncensored(unit.orig), unit.orig)
+	}
+}
