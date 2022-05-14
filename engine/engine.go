76ea51bc013fc2e2022f0bf52c964a6ab59101df
xjasonlyu <xjasonlyu@gmail.com>
2022-05-14 16:34:29 +0800

    update engine

--- engine/engine.go
+++ engine/engine.go
@@ -1,12 +1,23 @@
 package engine
 
 import (
+	"strings"
 	"time"
 
 	"github.com/javtube/javtube-sdk-go/model"
 	javtube "github.com/javtube/javtube-sdk-go/provider"
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+type Options struct {
+	// DSN the Data Source Name.
+	DSN string
+
+	// Timeout for each request.
+	Timeout time.Duration
+}
 
 type Engine struct {
 	db             *gorm.DB
@@ -14,7 +25,12 @@ type Engine struct {
 	actorProviders map[string]javtube.ActorProvider
 }
 
-func New(db *gorm.DB, timeout time.Duration) (engine *Engine) {
+func New(opts *Options) (engine *Engine, err error) {
+	var db *gorm.DB
+	if db, err = openDB(opts.DSN); err != nil {
+		return
+	}
+
 	engine = &Engine{
 		db:             db,
 		movieProviders: make(map[string]javtube.MovieProvider),
@@ -23,16 +39,16 @@ func New(db *gorm.DB, timeout time.Duration) (engine *Engine) {
 	// Initialize movie providers.
 	javtube.RangeMovieFactory(func(name string, factory javtube.MovieFactory) {
 		provider := factory()
-		if s, ok := provider.(javtube.RequestTimeoutSetter); ok && timeout > 0 {
-			s.SetRequestTimeout(timeout)
+		if s, ok := provider.(javtube.RequestTimeoutSetter); ok {
+			s.SetRequestTimeout(opts.Timeout)
 		}
 		engine.movieProviders[name] = provider
 	})
 	// Initialize actor providers.
 	javtube.RangeActorFactory(func(name string, factory javtube.ActorFactory) {
 		provider := factory()
-		if s, ok := provider.(javtube.RequestTimeoutSetter); ok && timeout > 0 {
-			s.SetRequestTimeout(timeout)
+		if s, ok := provider.(javtube.RequestTimeoutSetter); ok {
+			s.SetRequestTimeout(opts.Timeout)
 		}
 		engine.actorProviders[name] = factory()
 	})
@@ -45,6 +61,21 @@ func (e *Engine) AutoMigrate() error {
 		&model.ActorInfo{})
 }
 
-func (e *Engine) Download() {
+func openDB(dsn string) (*gorm.DB, error) {
+	var dialector gorm.Dialector
+	if dsn == "" {
+		// We use memory sqlite DB by default.
+		dsn = "file::memory:?cache=shared"
+	}
 
+	// We try to parse it as postgresql, otherwise
+	// fallback to sqlite.
+	if strings.HasPrefix(dsn, "postgresql://") ||
+		len(strings.Fields(dsn)) > 4 {
+		dialector = postgres.Open(dsn)
+	} else {
+		dialector = sqlite.Open(dsn)
+	}
+
+	return gorm.Open(dialector, &gorm.Config{})
 }
