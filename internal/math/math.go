7853736fdcfd005a3e6cc3075f5fed2f079c3734
xjasonlyu <xjasonlyu@gmail.com>
2022-05-20 18:49:57 +0800

    improve num

--- internal/math/math.go
+++ internal/math/math.go
@@ -1,13 +1,19 @@
 package math
 
-func Min[T int | float64](a, b T) T {
+type num interface {
+	int | int8 | int16 | int32 | int64 |
+		uint | uint8 | uint16 | uint32 | uint64 |
+		float32 | float64
+}
+
+func Min[T num](a, b T) T {
 	if a < b {
 		return a
 	}
 	return b
 }
 
-func Max[T int | float64](a, b T) T {
+func Max[T num](a, b T) T {
 	if a > b {
 		return a
 	}
