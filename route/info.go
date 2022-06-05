b06f3d2ac39d04547214b484cff84be03d3647a2
xjasonlyu <xjasonlyu@gmail.com>
2022-06-05 17:23:37 +0800

    rm translate in info

--- route/info.go
+++ route/info.go
@@ -6,8 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/javtube/javtube-sdk-go/engine"
-	"github.com/javtube/javtube-sdk-go/model"
-	"github.com/javtube/javtube-sdk-go/translate"
 )
 
 type infoType uint8
@@ -21,7 +19,6 @@ type infoQuery struct {
 	ID       string `form:"id"`
 	Provider string `form:"provider"`
 	URL      string `form:"url"`
-	Language string `form:"lang"`
 	Lazy     bool   `form:"lazy"`
 }
 
@@ -63,21 +60,6 @@ func getInfo(app *engine.Engine, typ infoType) gin.HandlerFunc {
 		if err != nil {
 			abortWithError(c, err)
 			return
-		}
-
-		// translate
-		if typ == movieInfoType && query.Language != "" {
-			info := info.(*model.MovieInfo)
-			if info.Title != "" {
-				if title, _ := translate.Translate(info.Title, "auto", query.Language); title != "" {
-					info.Title = title
-				}
-			}
-			if info.Summary != "" {
-				if summary, _ := translate.Translate(info.Summary, "auto", query.Language); summary != "" {
-					info.Summary = summary
-				}
-			}
 		}
 
 		// JSON reply.
