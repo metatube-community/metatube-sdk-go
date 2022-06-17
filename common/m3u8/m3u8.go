d349d0208932c32418e44f383449279206b52c7f
xjasonlyu <xjasonlyu@gmail.com>
2022-06-17 17:49:25 +0800

    Add m3u8 mod

---
+++ common/m3u8/m3u8.go
@@ -0,0 +1,38 @@
+package m3u8
+
+import (
+	"errors"
+	"io"
+	"sort"
+
+	"github.com/grafov/m3u8"
+)
+
+const (
+	MEDIA  = m3u8.MEDIA
+	MASTER = m3u8.MASTER
+)
+
+func ParseMediaURI(reader io.Reader) (string, m3u8.ListType, error) {
+	playList, listType, err := m3u8.DecodeFrom(reader, true)
+	if err != nil {
+		return "", 0, err
+	}
+	switch listType {
+	case m3u8.MEDIA:
+		return "" /* as is */, MEDIA, nil
+	case m3u8.MASTER:
+		masterPL := playList.(*m3u8.MasterPlaylist)
+		if len(masterPL.Variants) == 0 {
+			return "", MASTER, errors.New("no variants")
+		}
+		// sort by bandwidth.
+		sort.SliceStable(masterPL.Variants, func(i, j int) bool {
+			return masterPL.Variants[i].Bandwidth < masterPL.Variants[j].Bandwidth
+		})
+		// return uri with the highest bandwidth.
+		return masterPL.Variants[len(masterPL.Variants)-1].URI, MASTER, nil
+	default:
+		return "", 0, errors.New("bad list type")
+	}
+}
