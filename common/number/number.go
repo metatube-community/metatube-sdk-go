27281e12afb4fd859a2271c5661837cd1e699ac7
xjasonlyu <xjasonlyu@gmail.com>
2022-06-24 18:59:07 +0800

    support more number styles

--- common/number/number.go
+++ common/number/number.go
@@ -31,7 +31,7 @@ func Trim(s string) string {
 		ReplaceAllString(s, " " /* space */) // fix special chars.
 	s = regexp.MustCompile(`(?i)\s+-\s+`).
 		ReplaceAllString(s, " " /* space */) // fix `number - title` style
-	s = regexp.MustCompile(`(?i)[-_.](dvd|iso|mkv|mp4|c?avi|\d*fps|whole|(f|hhb)?hd\d*|sd\d*|(?:360|480|720|1080|2160)p|uncensored|leak|[2468]k|[xh]26[45])+|[\[(（].*[])）]`).
+	s = regexp.MustCompile(`(?i)[-_.](dvd|iso|mkv|mp4|c?avi|\d*fps|whole|(f|hhb)?hd\d*|sd\d*|(?:360|480|720|1080|2160)p|uncensored|leak|[2468]k|[xh]26[45])+|[\[【(（].*[]】)）]`).
 		ReplaceAllString(s, "") // trim tags
 	s = regexp.MustCompile(`(?i)[-_\s]*(carib(b?ean)?|1?Pondo?|10musume|pacopacomama|muramura|Tokyo[-_\s]?Hot)([-_\s]*|$)`).
 		ReplaceAllString(s, "") // trim prefixes
