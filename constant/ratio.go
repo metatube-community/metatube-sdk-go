6a21e2b4187b412e34fccf60d05726ddfe4bb30e
xjasonlyu <xjasonlyu@gmail.com>
2022-06-24 16:19:38 +0800

    Add PosterImageRatio

--- constant/ratio.go
+++ constant/ratio.go
@@ -3,6 +3,7 @@ package constant
 // NOTE: primary/thumb/backdrop represents MEDIA picture ratios.
 const (
 	PrimaryImageRatio  = 2.0 / 3.0
+	PosterImageRatio   = 7.0 / 10.0
 	ThumbImageRatio    = 16.0 / 9.0
 	BackdropImageRatio = 0 // no cropping
 )
