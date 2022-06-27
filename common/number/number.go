bbc6ae59b07b6b5079d6b1094741d8746bb1d6d9
xjasonlyu <xjasonlyu@gmail.com>
2022-06-27 18:43:26 +0800

    Improve number trimming

--- common/number/number.go
+++ common/number/number.go
@@ -4,45 +4,31 @@ import (
 	"path"
 	"regexp"
 	"strings"
-	"unicode"
 
 	"github.com/adrg/strutil"
 	"github.com/adrg/strutil/metrics"
 )
-
-func findFirstNonASCII(s string) int {
-	for i, r := range []rune(s) {
-		if r > unicode.MaxASCII {
-			return i
-		}
-	}
-	return len(s)
-}
 
 func Trim(s string) string {
 	const maxExtLength = 7
 	if ext := path.Ext(s); len(ext) < maxExtLength {
 		s = s[:len(s)-len(ext)] // trim extension
 	}
-	if ss := strings.Split(s, "@"); len(ss) > 0 {
-		s = ss[len(ss)-1] // trim @ char
+	s = regexp.MustCompile(`(?i)([a-z\d]+\.(?:com|net|top|xyz|tv))(?:[^a-z\d]|$)`).
+		ReplaceAllString(s, "") // trim domain
+	if ss := regexp.MustCompile(`(?i)([a-z\d]{2,}(?:[-_][a-z\d]{2,})+)`).FindStringSubmatch(s); len(ss) > 0 {
+		s = ss[1] // first find number with dashes
+	} else if ss = regexp.MustCompile(`(?i)([a-z]+\d+)`).FindStringSubmatch(s); len(ss) > 1 {
+		s = ss[1] // otherwise find number with alphas & digits
 	}
-	s = regexp.MustCompile(`(?i)[,;#]`).
-		ReplaceAllString(s, " " /* space */) // fix special chars
-	s = regexp.MustCompile(`(?i)\s+[-_+]\s+`).
-		ReplaceAllString(s, " " /* space */) // fix `number - title` style
-	s = regexp.MustCompile(`(?i)^(?:(?:f?hd|sd)[-_]|\d+\.)(.*$)`).
+	s = regexp.MustCompile(`(?i)^(?:f?hd|sd)[-_](.*$)`).
 		ReplaceAllString(s, "${1}") // trim special prefixes
-	s = regexp.MustCompile(`(?i)[-_.](dvd|iso|mkv|mp4|c?avi|\d*fps|whole|(f|hhb)?hd\d*|sd\d*|(?:360|480|720|1080|2160)p|uncensored|leak|[2468]k|[xh]26[45])+|[\[【(（].*?[]】)）]`).
+	s = regexp.MustCompile(`(?i)[-_.](dvd|iso|mkv|mp4|c?avi|\d*fps|whole|(f|hhb)?hd\d*|sd\d*|(?:360|480|720|1080|2160)p|uncensored|leak|[2468]k|[xh]26[45])+`).
 		ReplaceAllString(s, "") // trim tags
 	s = regexp.MustCompile(`(?i)[-_\s]*(carib(b?ean)?|1?Pondo?|10musume|pacopacomama|muramura|Tokyo[-_\s]?Hot)([-_\s]*|$)`).
 		ReplaceAllString(s, "") // trim makers
 	s = regexp.MustCompile(`^(?i)\s*(FC2[-_]?PPV)[-_]`).
 		ReplaceAllString(s, "FC2-") // normalize fc2 prefixes
-	s = s[:findFirstNonASCII(s)] // trim unicode content
-	if ss := strings.Fields(s); len(ss) > 0 {
-		s = ss[0] // trim possible alpha started title
-	}
 	for re := regexp.MustCompile(`(?i)([-_](c|ch|cd\d{1,2})|ch)\s*$`); re.MatchString(s); {
 		s = re.ReplaceAllString(s, "") // repeatedly trim suffixes
 	}
