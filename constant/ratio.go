2f0eb08bbe5fb3c1daed9b5525fb59c0651672de
xjasonlyu <xjasonlyu@gmail.com>
2022-07-05 17:07:34 +0800

    Rename ratios

--- constant/ratio.go
+++ constant/ratio.go
@@ -2,8 +2,9 @@ package constant
 
 // NOTE: primary/thumb/backdrop represents MEDIA picture ratios.
 const (
-	PrimaryImageRatio  = 2.0 / 3.0
-	PosterImageRatio   = 7.0 / 10.0
-	ThumbImageRatio    = 16.0 / 9.0
-	BackdropImageRatio = 0 // no cropping
+	PrimaryImageRatio   = 2.0 / 3.0
+	ThumbImageRatio     = 16.0 / 9.0
+	BackdropImageRatio  = OriginalImageRatio
+	OriginalImageRatio  = 0 // no cropping
+	JavPosterImageRatio = 7.0 / 10.0
 )
