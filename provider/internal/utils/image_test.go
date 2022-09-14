e13c7903aedc257d0fa7228f291299086f45049c
xjasonlyu <xjasonlyu@gmail.com>
2022-09-14 14:42:31 +0800

    Add similar test cases

---
+++ provider/internal/utils/image_test.go
@@ -0,0 +1,93 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSimilarImage(t *testing.T) {
+	for i, item := range []struct {
+		imgUrl1, imgUrl2 string
+	}{
+		// MSFH-025
+		{
+			"https://pics.dmm.co.jp/digital/video/1msfh00037/1msfh00025ps.jpg",
+			"https://pics.dmm.co.jp/digital/video/1msfh00037/1msfh00025jp-1.jpg",
+		},
+		// MSFH-037
+		{
+			"https://pics.dmm.co.jp/digital/video/1msfh00037/1msfh00037ps.jpg",
+			"https://pics.dmm.co.jp/digital/video/1msfh00037/1msfh00037jp-1.jpg",
+		},
+		// STARS-154
+		{
+			"https://pics.dmm.co.jp/digital/video/1stars00309/1stars00154ps.jpg",
+			"https://pics.dmm.co.jp/digital/video/1stars00309/1stars00154jp-1.jpg",
+		},
+		// STARS-249
+		{
+			"https://pics.dmm.co.jp/digital/video/1stars00309/1stars00249ps.jpg",
+			"https://pics.dmm.co.jp/digital/video/1stars00309/1stars00249jp-1.jpg",
+		},
+		// STARS-309
+		{
+			"https://pics.dmm.co.jp/digital/video/1stars00309/1stars00309ps.jpg",
+			"https://pics.dmm.co.jp/digital/video/1stars00309/1stars00309jp-1.jpg",
+		},
+		// STARS-325
+		{
+			"https://pics.dmm.co.jp/digital/video/1stars00309/1stars00325ps.jpg",
+			"https://pics.dmm.co.jp/digital/video/1stars00309/1stars00325jp-1.jpg",
+		},
+		// STARS-330
+		{
+			"https://pics.dmm.co.jp/digital/video/1stars00309/1stars00330ps.jpg",
+			"https://pics.dmm.co.jp/digital/video/1stars00309/1stars00330jp-1.jpg",
+		},
+		// SDMF-011
+		{
+			"https://pics.dmm.co.jp/digital/video/1sdmf00011/1sdmf00011ps.jpg",
+			"https://pics.dmm.co.jp/digital/video/1sdmf00011/1sdmf00011jp-1.jpg",
+		},
+		//{
+		//	"https://pics.dmm.co.jp/digital/video/1sdmf00011/1sdmf00011ps.jpg",
+		//	"https://pics.dmm.co.jp/digital/video/1sdmf00011/1sdmf00011jp-2.jpg",
+		//},
+		// SDMF-018
+		{
+			"https://pics.dmm.co.jp/digital/video/1sdmf00011/1sdmf00018ps.jpg",
+			"https://pics.dmm.co.jp/digital/video/1sdmf00011/1sdmf00018jp-1.jpg",
+		},
+		// SDMF-023
+		{
+			"https://pics.dmm.co.jp/digital/video/1sdmf00011/1sdmf00023ps.jpg",
+			"https://pics.dmm.co.jp/digital/video/1sdmf00011/1sdmf00023jp-1.jpg",
+		},
+		// STARS-200
+		{
+			"https://pics.dmm.co.jp/digital/video/1stars00200/1stars00200ps.jpg",
+			"https://pics.dmm.co.jp/digital/video/1stars00200/1stars00200jp-1.jpg",
+		},
+		//{
+		//	"https://pics.dmm.co.jp/digital/video/1stars00200/1stars00200ps.jpg",
+		//	"https://pics.dmm.co.jp/digital/video/1stars00200/1stars00200jp-2.jpg",
+		//},
+		//{
+		//	"https://pics.dmm.co.jp/digital/video/1stars00200/1stars00200jp-1.jpg",
+		//	"https://pics.dmm.co.jp/digital/video/1stars00200/1stars00200jp-2.jpg",
+		//},
+		//{
+		//	"https://pics.dmm.co.jp/digital/video/1stars00200/1stars00200pl.jpg",
+		//	"https://pics.dmm.co.jp/digital/video/1stars00200/1stars00200jp-1.jpg",
+		//},
+	} {
+		v := SimilarImage(item.imgUrl1, item.imgUrl2, nil)
+		if v {
+			t.Logf("No. %d is similar.", i)
+		} else {
+			t.Logf("No. %d is distinct.", i)
+		}
+		assert.True(t, v)
+	}
+}
