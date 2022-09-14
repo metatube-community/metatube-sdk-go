c0ffe19b4c2366d805cd2a9013ffc09db4457204
xjasonlyu <xjasonlyu@gmail.com>
2022-09-14 12:29:13 +0800

    minor renames

--- imageutil/similarity.go
+++ imageutil/similarity.go
@@ -7,14 +7,14 @@ import (
 )
 
 // Similar is a shortcut of images4.Similar.
-func Similar(img1, img2 image.Image) bool {
+func Similar(imgA, imgB image.Image) bool {
 	// Icons are compact image representations (image "hashes").
 	// Name "hash" is not used intentionally.
-	icon1 := images4.Icon(img1)
-	icon2 := images4.Icon(img2)
+	iconA := images4.Icon(imgA)
+	iconB := images4.Icon(imgB)
 
 	// Comparison.
 	// Images are not used directly. Icons are used instead,
 	// because they have tiny memory footprint and fast to compare.
-	return images4.Similar(icon1, icon2)
+	return images4.Similar(iconA, iconB)
 }
