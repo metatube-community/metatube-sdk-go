b3bd85cffc55228a8fb8651498661234928e2d3f
xjasonlyu <xjasonlyu@gmail.com>
2022-09-15 15:35:20 +0800

    refactor: rename to hash

--- imageutil/hash.go
+++ imageutil/hash.go
@@ -12,37 +12,36 @@ const (
 	thPerceptionHash = 6
 )
 
-// Similar is a shortcut of images4.Similar.
-func Similar(imgA, imgB image.Image) bool {
-	switch {
-	case averageHashDistance(imgA, imgB) < thAverageHash:
-		return true
-	case differenceHashDistance(imgA, imgB) < thDifferenceHash:
-		return true
-	case perceptionHashDistance(imgA, imgB) < thPerceptionHash:
-		return true
-	default:
-		return false
-	}
-}
-
-func averageHashDistance(imgA, imgB image.Image) (distance int) {
+func AverageHashDistance(imgA, imgB image.Image) (distance int) {
 	hashA, _ := goimagehash.AverageHash(imgA)
 	hashB, _ := goimagehash.AverageHash(imgB)
 	distance, _ = hashA.Distance(hashB)
 	return
 }
 
-func differenceHashDistance(imgA, imgB image.Image) (distance int) {
+func DifferenceHashDistance(imgA, imgB image.Image) (distance int) {
 	hashA, _ := goimagehash.DifferenceHash(imgA)
 	hashB, _ := goimagehash.DifferenceHash(imgB)
 	distance, _ = hashA.Distance(hashB)
 	return
 }
 
-func perceptionHashDistance(imgA, imgB image.Image) (distance int) {
+func PerceptionHashDistance(imgA, imgB image.Image) (distance int) {
 	hashA, _ := goimagehash.PerceptionHash(imgA)
 	hashB, _ := goimagehash.PerceptionHash(imgB)
 	distance, _ = hashA.Distance(hashB)
 	return
+}
+
+func Similar(imgA, imgB image.Image) bool {
+	switch {
+	case AverageHashDistance(imgA, imgB) < thAverageHash:
+		return true
+	case DifferenceHashDistance(imgA, imgB) < thDifferenceHash:
+		return true
+	case PerceptionHashDistance(imgA, imgB) < thPerceptionHash:
+		return true
+	default:
+		return false
+	}
 }
