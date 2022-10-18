857743916914f0a302e1698051925205dc85e901
zijiren233 <pyh1670605849@gmail.com>
2022-10-18 16:52:45 +0800

    Fix: parse sup lang

--- translate/deepl.go
+++ translate/deepl.go
@@ -2,14 +2,13 @@ package translate
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strings"
 
 	"github.com/javtube/javtube-sdk-go/common/fetch"
 )
 
-const deelpTranslateAPI = "https://api-free.deepl.com/v2/translate"
+const deeplTranslateAPI = "https://api-free.deepl.com/v2/translate"
 
 type deeplData struct {
 	Translations []struct {
@@ -21,14 +20,14 @@ type deeplData struct {
 func DeeplTranslate(q, source, target, key string) (result string, err error) {
 	var resp *http.Response
 	if resp, err = fetch.Post(
-		deelpTranslateAPI,
+		deeplTranslateAPI,
 		fetch.WithURLEncodedBody(map[string]string{
 			"text":            q,
 			"source_lang":     parseToDeeplSupportedLanguage(source),
 			"target_lang":     parseToDeeplSupportedLanguage(target),
 			"split_sentences": "0", // disable stntence split
 		}),
-		fetch.WithRaiseForStatus(false),
+		fetch.WithRaiseForStatus(true),
 		fetch.WithHeader("Authorization", "DeepL-Auth-Key "+key),
 		fetch.WithHeader("Content-Type", "application/x-www-form-urlencoded"),
 	); err != nil {
@@ -38,51 +37,17 @@ func DeeplTranslate(q, source, target, key string) (result string, err error) {
 
 	data := new(deeplData)
 	if err = json.NewDecoder(resp.Body).Decode(data); err == nil {
-		if len(data.Translations) == 0 {
-			err = fmt.Errorf("translate text is nil: %v", data.Translations)
-			return
-		}
 		result = data.Translations[0].Text
 	}
 	return
 }
 
 func parseToDeeplSupportedLanguage(lang string) string {
-	lang = strings.ToLower(lang)
+	lang = strings.ToUpper(lang)
 	switch lang {
-	case "zh", "chs", "zh-cn", "zh_cn", "zh-hans":
+	case "ZH", "CHS", "ZH-CN", "ZH-HANS", "CHT", "ZH-TW", "ZH-HK", "ZH-HANT":
 		return "ZH"
-	case "cht", "zh-tw", "zh_tw", "zh-hk", "zh_hk", "zh-hant":
-		return "ZH"
-	case "jp", "ja":
-		return "JA"
-	case "kor", "ko":
-		return ""
-	case "vie", "vi":
-		return ""
-	case "spa", "es":
-		return "EL"
-	case "de":
-		return "DE"
-	case "fra", "fr":
-		return "FR"
-	case "ara", "ar":
-		return ""
-	case "bul", "bg":
-		return "BG"
-	case "est", "et":
-		return "ET"
-	case "dan", "da":
-		return "DA"
-	case "fin", "fi":
-		return ""
-	case "rom", "ro":
-		return "RO"
-	case "slo", "sl":
-		return "SL"
-	case "swe", "sv":
-		return "SV"
 	default:
-		return ""
+		return lang
 	}
 }
