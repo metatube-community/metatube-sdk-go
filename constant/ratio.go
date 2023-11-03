ac71d260f482f93ffbd130bd7cd8ac689839356e
Jason Lyu <xjasonlyu@gmail.com>
2023-11-04 04:47:19 +0800

    Chore: set Ratio to float64 (#19)

--- constant/ratio.go
+++ constant/ratio.go
@@ -2,7 +2,7 @@ package constant
 
 // NOTE: primary/thumb/backdrop represents MEDIA picture ratios.
 const (
-	PrimaryImageRatio  = 2.0 / 3.0
-	ThumbImageRatio    = 16.0 / 9.0
-	BackdropImageRatio = 0 // no cropping
+	PrimaryImageRatio  float64 = 2.0 / 3.0
+	ThumbImageRatio    float64 = 16.0 / 9.0
+	BackdropImageRatio float64 = 0 // no cropping
 )
