47395214c27e1376d3c7c5174507d01f2cc0ac93
Jason Lyu <xjasonlyu@gmail.com>
2023-11-12 05:53:23 +0800

    Fix: force limit jav321 timeout to 10s (#21)

--- provider/jav321/jav321.go
+++ provider/jav321/jav321.go
@@ -7,6 +7,7 @@ import (
 	"path"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/gocolly/colly/v2"
 	"golang.org/x/net/html"
@@ -40,6 +41,10 @@ type JAV321 struct {
 
 func New() *JAV321 {
 	return &JAV321{scraper.NewDefaultScraper(Name, baseURL, Priority)}
+}
+
+func (jav *JAV321) SetRequestTimeout(_ time.Duration) {
+	jav.Scraper.SetRequestTimeout(10 * time.Second)
 }
 
 func (jav *JAV321) GetMovieInfoByID(id string) (info *model.MovieInfo, err error) {
