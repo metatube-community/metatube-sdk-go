9c3dfef153813f8a52f347e9d5ab86bdda6de954
xjasonlyu <xjasonlyu@gmail.com>
2024-08-07 21:23:00 -0400

    Refactor: restruct cmd/server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
 	"github.com/metatube-community/metatube-sdk-go/cmd"
+	"github.com/metatube-community/metatube-sdk-go/engine"
+	V "github.com/metatube-community/metatube-sdk-go/internal/version"
 )
 
+func showVersionAndExit() {
+	fmt.Println(V.BuildString())
+	os.Exit(0)
+}
+
 func main() {
-	cmd.Main()
+	if _, isSet := os.LookupEnv("VERSION"); cmd.Config.VersionFlag &&
+		!isSet /* NOTE: ignore this flag if ENV contains VERSION variable. */ {
+		showVersionAndExit()
+	}
+
+	var (
+		addr = net.JoinHostPort(
+			cmd.Config.Bind,
+			cmd.Config.Port)
+		router = cmd.Router(engine.DefaultEngineName)
+	)
+	if err := http.ListenAndServe(addr, router); err != nil {
+		log.Fatal(err)
+	}
 }
