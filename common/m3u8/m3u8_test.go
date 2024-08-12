ec249a24961d7a26ff30e34259d825d85f9f1ed0
xjasonlyu <xjasonlyu@gmail.com>
2024-08-12 16:06:50 -0400

    up url

--- common/m3u8/m3u8_test.go
+++ common/m3u8/m3u8_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestParseBestMediaURI(t *testing.T) {
-	url := "https://ppvclips02.aventertainments.com/01m3u8/mmdv-120/mmdv-120.m3u8"
+	url := "http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8"
 	resp, err := fetch.Fetch(url)
 	if assert.NoError(t, err) {
 		defer resp.Body.Close()
