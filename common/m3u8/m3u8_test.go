b08a726c73ad95bc9497439cf43d688747bcd648
Jason Lyu <xjasonlyu@gmail.com>
2024-08-12 16:18:29 -0400

    CI(actions): add testing (#83)

--- common/m3u8/m3u8_test.go
+++ common/m3u8/m3u8_test.go
@@ -3,12 +3,16 @@ package m3u8
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/metatube-community/metatube-sdk-go/common/fetch"
 )
 
 func TestParseBestMediaURI(t *testing.T) {
-	url := "https://ppvclips02.aventertainments.com/01m3u8/mmdv-120/mmdv-120.m3u8"
-	resp, _ := fetch.Fetch(url)
-	defer resp.Body.Close()
-	t.Log(ParseBestMediaURI(resp.Body))
+	url := "http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8"
+	resp, err := fetch.Fetch(url)
+	if assert.NoError(t, err) {
+		defer resp.Body.Close()
+		t.Log(ParseBestMediaURI(resp.Body))
+	}
 }
