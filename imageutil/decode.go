224000b48697d6f241ba508a9530fb231073c5a4
xjasonlyu <xjasonlyu@gmail.com>
2024-09-19 20:45:07 -0400

    Feature: support jpegli image decoding
    minor

--- imageutil/decode.go
+++ imageutil/decode.go
@@ -1,10 +1,33 @@
 package imageutil
 
 import (
+	"bytes"
+	"errors"
 	"image"
+	"image/jpeg"
+	"io"
+
+	"github.com/gen2brain/jpegli"
 )
 
-var (
-	Decode       = image.Decode
-	DecodeConfig = image.DecodeConfig
-)
+// Ref: https://github.com/gen2brain/jpegli/issues/4
+func init() {
+	defer func() {
+		recover()
+	}()
+	_, _ = jpegli.Decode(&bytes.Buffer{})
+}
+
+func Decode(r io.Reader) (image.Image, string, error) {
+	buf, err := io.ReadAll(r)
+	if err != nil {
+		return nil, "", err
+	}
+	var jpegErr jpeg.UnsupportedError
+	m, f, err := image.Decode(bytes.NewBuffer(buf))
+	if err != nil && errors.As(err, &jpegErr) {
+		// Fallback to decode with jpegli.
+		m, err = jpegli.Decode(bytes.NewBuffer(buf))
+	}
+	return m, f, err
+}
