0256fea0d7516d76ad8aa5c26254ab372edc06cc
hollc <naimoreal99@gmail.com>
2025-01-16 13:29:24 +0800

    Add site for searching fc2 movies

--- engine/register.go
+++ engine/register.go
@@ -28,6 +28,7 @@ import (
 	_ "github.com/metatube-community/metatube-sdk-go/provider/heyzo"
 	_ "github.com/metatube-community/metatube-sdk-go/provider/jav321"
 	_ "github.com/metatube-community/metatube-sdk-go/provider/javbus"
+	_ "github.com/metatube-community/metatube-sdk-go/provider/javfree"
 	_ "github.com/metatube-community/metatube-sdk-go/provider/kin8tengoku"
 	_ "github.com/metatube-community/metatube-sdk-go/provider/mgstage"
 	_ "github.com/metatube-community/metatube-sdk-go/provider/muramura"
