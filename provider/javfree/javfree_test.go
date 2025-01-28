4e79ece5a8d73cfd801f49ff312ff0a8784ad973
holll <36074987+holll@users.noreply.github.com>
2025-01-28 10:55:14 +0800

    Feature: add a `fc2` site (#150)

---
+++ provider/javfree/javfree_test.go
@@ -0,0 +1,19 @@
+package javfree
+
+import (
+	"testing"
+
+	"github.com/metatube-community/metatube-sdk-go/provider/internal/testkit"
+)
+
+func TestJAVFREE_GetMovieInfoByID(t *testing.T) {
+	testkit.Test(t, New, []string{
+		"243452-1151912",
+	})
+}
+
+func TestJAVFREE_SearchMovie(t *testing.T) {
+	testkit.Test(t, New, []string{
+		"FC2-PPV-1151912",
+	})
+}
