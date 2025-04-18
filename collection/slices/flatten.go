9a8cdc64e1c5ea9a0a148b22218150865dd40d70
xjasonlyu <xjasonlyu@gmail.com>
2025-04-18 19:31:35 -0400

    comment

--- collection/slices/flatten.go
+++ collection/slices/flatten.go
@@ -4,8 +4,7 @@ import (
 	"slices"
 )
 
-// Flatten flattens a 2D slice into a 1D
-// slice by merging all inner slices.
+// Flatten flattens a 2D slice into a 1D slice by merging all inner slices.
 func Flatten[E any](s [][]E) []E {
 	return slices.Collect(func(yield func(E) bool) {
 		for _, i := range s {
